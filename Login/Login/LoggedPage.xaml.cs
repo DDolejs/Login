e2564a6ce45f02c49c323fc8d743c8d613753e9b
Dolejš Daniel <dolejs.daniel.2017@skola.ssps.cz>
2020-12-03 22:01:05 +0100

    Přidejte soubory projektu.

---
+++ Login/Login/LoggedPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoggedPage : ContentPage
+    {
+        public LoggedPage()
+        {
+            InitializeComponent();
+           
+        }
+        public LoggedPage(LogInfo li)
+        {
+            InitializeComponent();
+            BindingContext = li;
+        }
+
+        
+    }
+}
---
+++ Login/Login/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace Login
+{
+    public class LogInfo
+    {
+       public string Name { get; set; }     
+    }
+    
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private LogInfo li { get; set; }
+       public string Jmeno { get; set; }
+       
+        public MainPage()
+        {
+            InitializeComponent();
+            li = new LogInfo();
+            BindingContext = li;
+        }
+      
+        private void butt_Clicked(object sender, EventArgs e)
+        {
+            if (!(pass.Text.Any(char.IsDigit)))
+            {
+                DisplayAlert("Neplatné heslo", "Heslo neobsahuje číslo.", "OK");
+            }
+            else if (!(pass.Text.Length > 8))
+            {
+                DisplayAlert("Neplatné heslo", "Heslo nemá požadovanou délku", "OK");
+            }
+
+            else 
+            {
+                Page p = new LoggedPage();
+                Application.Current.MainPage.Navigation.PushAsync(new NavigationPage(p));
+                
+            }
+        }
+    }
+}
